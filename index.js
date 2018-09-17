caf6c240f30877ecc4f0eb7b12966482d0f4b8bd
kumarmadhavnarayan <kumarmadhavnarayan@gmail.com>
2018-09-17 06:04:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+const verificationController=require('./controllers/verification');
+const messageWebhookController = require('./Helpers/messageWebhook');
+app.get('/', verificationController);
+app.post('/', messageWebhookController);
